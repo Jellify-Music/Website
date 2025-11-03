90ec3c9f7c7dfaf789a1a73af7985372f1c7eafc
Violet Caulfield <42452695+anultravioletaurora@users.noreply.github.com>
2025-11-03 07:20:51 -0600

    Add Tamagui to the project
    Enables consistent styling between the app and the marketing website

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,25 @@
 import type { NextConfig } from "next";
+import { defaultConfig } from "@tamagui/config/v4";
+import { withTamagui } from "@tamagui/next-plugin";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const tamaguiPlugin = withTamagui({
+  config: './tamagui.config.ts',
+  components: ['tamagui']
+})
+
+const jellifyConfig = {
+  ...defaultConfig
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  ...jellifyConfig,
+  ...tamaguiPlugin(nextConfig)
+};
 
 // added by create cloudflare to enable calling `getCloudflareContext()` in `next dev`
 import { initOpenNextCloudflareForDev } from '@opennextjs/cloudflare';
